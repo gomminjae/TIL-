efed465fa891e2ba3bfb5609b9604d7c55a16dcb
gomminjae <48856104+gomminjae@users.noreply.github.com>
2020-01-18 18:30:32 +0900

    Add files via upload

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CollectionviewTest
+//
+//  Created by Apple on 2020/01/18.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
+
